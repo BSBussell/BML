36b20257793d1fb878d38aa2a6195806e008ebc5
Bee Bussell <benjaminsbussell@gmail.com>
2021-09-03 11:27:15 -0400

    Setting up for keyboard inputs

--- src/bEvent.cpp
+++ src/bEvent.cpp
@@ -12,9 +12,24 @@ bool bEvent::eventLoop() {
     while( SDL_PollEvent( &event ) ) {
 
         switch(event.type) {
+            
             case SDL_QUIT:
                 return false;
+            
+            case SDL_KEYDOWN:
+
+                break;
+
+            case SDL_KEYUP:
+                
+                break;
+            
+            case SDL_WINDOWEVENT:
+
+                break;
+
             default:
+                printf("Look: %u\n", event.type);
                 break;
         }
     }
--- src/bEvent.h
+++ src/bEvent.h
@@ -14,6 +14,17 @@ class bEvent {
 public:
     static bool eventLoop();
 
+    static bool KeyDown(const char *key );
+    static bool KeyStillDown(const char *key );
+    static bool KeyUp(const char *key );
+    static bool KeyStillUp(const char *key );
+    /* 
+    static bool MouseDown(const char *key );
+    static bool MouseStillDown(const char *key );
+    static bool MouseUp(const char *key );
+    static bool MouseStillUp(const char *key );
+    */
+    
 private:
     
     bEvent();
