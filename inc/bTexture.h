2220453d2f410f0a1918c2dd9f39163865603dbd
Bee Bussell <BSBussell@users.noreply.github.com>
2023-03-09 12:04:26 -0500

    Considering Strategies

--- inc/bTexture.h
+++ inc/bTexture.h
@@ -1,49 +1,66 @@
 
 // Bee Bussell
 // Sept 6, 2021
-// Header
+// bTexture/manager
 
 #ifndef BML_TEXTURE_H
 #define BML_TEXTURE_H
 
-//#include <string>
+#include <memory>
+#include <string>
 #include <unordered_map>
-#include <SDL2/SDL.h>
 
-// Just a header file for now
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+
+#include "bRect.h"
 
 struct bTexture {
 
+	~bTexture();
+
 	std::string path;
 
-	SDL_Texture* texture;
+	SDL_Texture *texture;
 	SDL_Rect src;
+
 };
 
 class bTextureManager {
 
 public:
 
-	bTextureManager(SDL_Renderer renderer);
+	bTextureManager(SDL_Renderer *renderer);
 	~bTextureManager();
 
 	/* 
-		Ok so load_texture will check the path with loaded_textures,
+		Ok so load_texture will check the path with _loaded_textures,
 		if it finds that it has loaded it already it will
-		Make a new bTexture with the src, copy the SDL_Texture and return it
-		Otherwise, 
+		Make a new bTexture with the src, copy the SDL_Texture from
+		_loaded_textures increment the _refs value keyed on the texture
+		and return it.
+		Otherwise, completely make a new texture setup the slot in _refs 
+		and _loaded_textures
 	*/
-	bTexture *loadTexture(const char *path, bRect dim);
-	void unloadTexture(bTexture *texture);
 
+	bTexture loadTexture(const char *path, bRect dim);
+	void unloadTexture(bTexture texture);
+
+	// Does not delete bTextures, User still has to do that on ther own
 	void clearCache();
+
+	void renderTexture(bTexture &texture, bRect dest);
 
 private:
 
 	SDL_Renderer *_sdl_renderer;
 
+
 	std::unordered_map<std::string, SDL_Texture*> _loaded_textures;
 	std::unordered_map<SDL_Texture*, Uint8> _refs;
-}
+
+	//std::unordered_map<SDL_Texture*, bTexture*> _b_textures;
+
+};
 
 #endif
--- src/bTexture.cpp
+++ src/bTexture.cpp
@@ -3,8 +3,9 @@
 // For Later
 
 #include "bTexture.h"
+#include "BML.h"
 
-bTextureManager::bTextureManager(SDL_Renderer renderer) {
+bTextureManager::bTextureManager(SDL_Renderer *renderer) {
 
 	_sdl_renderer = renderer;
 }
@@ -15,12 +16,12 @@ bTextureManager::~bTextureManager() {
 }
 
 // Load the texture either by creating a new one or grabbing it from the cache
-bTexture *bTextureManager::loadTexture(const char *path, bRect dim) {
+bTexture bTextureManager::loadTexture(const char *path, bRect dim) {
 
 	std::string absPath = BML_GetPath(path);
 
 	// No matter what we're make a new bTexture
-	bTexture newTexture = new bTexture();
+	bTexture newTexture;// = new bTexture();
 	newTexture.src = dim;
 	newTexture.path = absPath;
 
@@ -46,18 +47,27 @@ bTexture *bTextureManager::loadTexture(const char *path, bRect dim) {
     // Setup our look ups and reference counter
     _loaded_textures[absPath] = newTexture.texture;
     _refs[newTexture.texture] = 1;
+
+    return newTexture;
 }
 
 // Kinda do the opposite of the above
-void unloadTexture(bTexture *texture) {
+void bTextureManager::unloadTexture(bTexture texture) {
 
 	// Decrease the ref count
-	auto refs_iter = _refs.find(texture->texture);
+	auto refs_iter = _refs.find(texture.texture);
+	
+	// If it's not in the map just delete the bTexture
+	if (refs_iter == _refs.end()) {
+		//delete texture;
+		return;
+	}
+
 	refs_iter->second--;
 
 	// If the ref count is zero, delete it from the cache and free memory
 	if (refs_iter->second == 0) {
-		auto loaded_iter = _loaded_textures.find(texture->path);
+		auto loaded_iter = _loaded_textures.find(texture.path);
 		if (loaded_iter != _loaded_textures.end()) {
 			SDL_DestroyTexture(loaded_iter->second);
 			_loaded_textures.erase(loaded_iter);
@@ -66,11 +76,11 @@ void unloadTexture(bTexture *texture) {
 	}
 
 	// Free the bTexture memory
-	delete texture;
+	//delete texture;
 }
 
 // Clear the maps and free all memory, extremely dangerous to use,,, please be careful
-void clearCache() {
+void bTextureManager::clearCache() {
 
 	// Delete all loaded textures and reset the reference counters
 	for (auto& texture : _loaded_textures) {
@@ -79,4 +89,23 @@ void clearCache() {
 	_loaded_textures.clear();
 	_refs.clear();
 
+}
+
+void bTextureManager::renderTexture(bTexture &texture, bRect dest) {
+
+	// Make sure the SDL_Texture hasn't been unloaded
+	auto refs_iter = _refs.find(texture.texture);
+	if (refs_iter == _refs.end()) {
+
+		// Create new texture
+		SDL_Surface* surface = IMG_Load(texture.path.c_str());
+	    texture.texture = SDL_CreateTextureFromSurface(_sdl_renderer, surface);
+	    SDL_FreeSurface(surface);
+
+	    // Setup our look ups and reference counter
+	    _loaded_textures[texture.path] = texture.texture;
+	    _refs[texture.texture] = 1;	
+	}
+
+	SDL_RenderCopy(_sdl_renderer, texture.texture, &texture.src, &dest);
 }
